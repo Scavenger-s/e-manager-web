fb21e4aa764f43268273f4938c77ba7a70bdbacd
Scavenger-s <1106042046@qq.com>
2020-04-19 22:40:47 +0800

    4.19

---
+++ src/main/java/com/cusx/controller/ContentController.java
@@ -0,0 +1,31 @@
+package com.cusx.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cusx.commons.utils.E3Result;
+import com.cusx.content.service.ContentService;
+import com.cusx.pojo.TbContent;
+
+
+/**
+ * 内容管理Controller
+ * @version 1.0
+ */
+@Controller
+public class ContentController {
+	
+	@Autowired
+	private ContentService contentService;
+
+	@RequestMapping(value="/content/save", method=RequestMethod.POST)
+	@ResponseBody
+	public E3Result addContent(TbContent content) {
+		//调用服务把内容数据保存到数据库
+		E3Result e3Result = contentService.addContent(content);
+		return e3Result;
+	}
+}
